0caa9f3a40201dbaa44ab5edb282ee3deab3a88d
mehrnoosh <mehr910093@gmail.com>
2022-08-20 09:53:51 +0430

    Add metadata and assests

---
+++ pages/posts/first-post.jsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+
+export default function FirstPost() {
+  return (
+    <div className="container">
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        // strategy controls when the third-party script should load
+        // A value of lazyOnload tells Next.js to load this particular script lazily during browser idle time
+        strategy="lazyOnload"
+        // run any JavaScript code immediately after the script has finished loading
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <main>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </main>
+    </div>
+  );
+}
